da39bc9f49c7c9a0bad753e9398fd86dc86c699f
jmagni2 <32044377+jmagni2@users.noreply.github.com>
2022-01-12 16:36:04 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 import { Routes, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/shop' element={<ShopPage />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shop' element={<ShopPage />} />
+        </Routes>
+      </div>
     );
   }
 }
